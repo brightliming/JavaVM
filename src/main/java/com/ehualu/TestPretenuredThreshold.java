f7c0f50b6ed38a99f4b62a48a2f668e40a9f5e03
limingbright <limingbright@yahoo.com>
2017-08-30 16:58:25 +0800

    'testtest'

---
+++ src/main/java/com/ehualu/TestPretenuredThreshold.java
@@ -0,0 +1,18 @@
+package com.ehualu;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+public class TestPretenuredThreshold {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void testPretenuredTHreshold(){
+        byte[] allocation;
+        allocation = new byte[4 * _1MB];
+    }
+
+    public static void main(String[] args){
+        testPretenuredTHreshold();
+    }
+}
